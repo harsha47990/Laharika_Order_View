7efc7cda53dbf72a432c2b5a79d367dda8caeb1f
harsha47990 <harsha.martha@gmail.com>
2021-12-19 19:32:03 +0530

    intial1

---
+++ File Manager.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Laharika_File_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBox_Comments = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBox_Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox_Order_ID = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.NotifyFilter = System.IO.NotifyFilters.FileName;
+            this.fileSystemWatcher1.Path = "D:\\";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.TextBox_Comments);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.TextBox_Name);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TextBox_Order_ID);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(763, 69);
+            this.panel1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(638, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Refresh);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(539, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Update Comments";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.UpdateComment_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Comments";
+            // 
+            // TextBox_Comments
+            // 
+            this.TextBox_Comments.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TextBox_Comments.Location = new System.Drawing.Point(98, 42);
+            this.TextBox_Comments.Name = "TextBox_Comments";
+            this.TextBox_Comments.Size = new System.Drawing.Size(429, 20);
+            this.TextBox_Comments.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(513, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // TextBox_Name
+            // 
+            this.TextBox_Name.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TextBox_Name.Location = new System.Drawing.Point(323, 11);
+            this.TextBox_Name.Name = "TextBox_Name";
+            this.TextBox_Name.Size = new System.Drawing.Size(176, 20);
+            this.TextBox_Name.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Order ID";
+            // 
+            // TextBox_Order_ID
+            // 
+            this.TextBox_Order_ID.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TextBox_Order_ID.Location = new System.Drawing.Point(87, 11);
+            this.TextBox_Order_ID.Name = "TextBox_Order_ID";
+            this.TextBox_Order_ID.Size = new System.Drawing.Size(178, 20);
+            this.TextBox_Order_ID.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(763, 330);
+            this.panel2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 330);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(763, 399);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Order Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBox_Order_ID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBox_Name;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBox_Comments;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ File Manager.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laharika_File_Management
+{
+    public partial class Form1 : Form
+    {
+        private static string orderid;
+        private static string OrderDetailsPath, Filter, PopUpNotification, AllowAppClosing,
+                              UpdateMsge, copyOnEnter, OrderFilesPath, TodayFolderPathLocal;
+        public static DataTable gridviewdata = new DataTable();
+        public Form1()
+        {
+            ReadConfigurations();
+            InitializeComponent();
+        }
+
+        private void ReadConfigurations()
+        {
+            OrderDetailsPath = ConfigurationManager.AppSettings["OrderDetailsPath"].ToString();
+            Filter = ConfigurationManager.AppSettings["Filter"].ToString();
+            PopUpNotification = ConfigurationManager.AppSettings["PopUpNotification"].ToString();
+            AllowAppClosing = ConfigurationManager.AppSettings["AllowAppClosing"].ToString();
+            UpdateMsge = ConfigurationManager.AppSettings["UpdateMsge"].ToString();
+            copyOnEnter = ConfigurationManager.AppSettings["CopyOnEnter"].ToString();
+            OrderFilesPath = ConfigurationManager.AppSettings["OrderFilesPath"].ToString();
+            TodayFolderPathLocal = ConfigurationManager.AppSettings["TodayFolderPathLocal"].ToString();
+            
+        }
+
+        private void CustomMsgBox(string msg)
+        {
+            Form frm = new Form();
+            frm.Text = "Message";
+            Label lb = new Label();
+            lb.Text = msg;
+            lb.Font = new Font("Arial", 15, FontStyle.Bold);
+            lb.TextAlign = ContentAlignment.MiddleCenter;
+            lb.Dock = DockStyle.Fill;
+            frm.BackColor = Color.White;
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            //frm.Size = new Size(300, 100);
+            frm.AutoSize = true;
+            frm.Controls.Add(lb);
+            frm.TopMost = true;
+            frm.Show();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileSystemWatcher1.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fileSystemWatcher1.Path = OrderDetailsPath;
+
+         
+            gridviewdata.Columns.Add("Order ID");
+            gridviewdata.Columns.Add("Folder Name");
+            gridviewdata.Columns.Add("Files Count");
+            gridviewdata.Columns.Add("Status");
+            gridviewdata.Columns.Add("Comments");
+            ReadOrders();
+
+        }
+
+        private void ReadOrders()
+        {
+            gridviewdata.Clear();
+            string[] files = Directory.GetFiles(OrderDetailsPath, $"*{Filter}.txt", SearchOption.AllDirectories);
+              
+            string order, folder="", count="", status="", comments="";
+
+            foreach(var file in files)
+            {
+                comments = "";
+                order = Path.GetFileName(file).Split('$')[0];
+                status = Path.GetFileNameWithoutExtension(file).Split('$')[1].Substring(2);
+                string[] data = File.ReadAllLines(file);
+                if (data.Length >= 3)
+                {
+                    folder = data[0].Split(':')[1];
+                    count = data[1].Split(':')[1];
+                    if (data.Length > 3)
+                    {
+                        comments = data[3].Split(':')[1];
+                    }
+                }
+               
+                gridviewdata.Rows.Add(order,folder,count, status, comments);
+            }
+            orderid = gridviewdata.Rows[0][0].ToString();
+            dataGridView1.DataSource = gridviewdata;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Update();
+            GC.Collect();
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+
+
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            int a, b;
+            while (true)
+            {
+                try
+                {
+                    a = File.ReadLines(e.FullPath).Count();
+                    Thread.Sleep(5000);
+                    b = File.ReadLines(e.FullPath).Count();
+                    if (a == b)
+                    { break;}
+                }
+                catch {  }
+            }
+
+            if(PopUpNotification =="False")
+            {
+                return;
+            }
+            if (Path.GetFileNameWithoutExtension(e.Name).Contains(Filter))
+            {
+                string order = Path.GetFileNameWithoutExtension(e.Name).Split('$')[0];
+                order = "Order No:- " + order;
+                ReadOrders();
+                CustomMsgBox(order);
+            }
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            e.Cancel = false;
+            dataGridView1.AllowUserToDeleteRows = true;
+            var rows = dataGridView1.SelectedRows;
+             foreach(var row in rows)
+            {
+                string order = gridviewdata.Rows[e.Row.Index]["Order ID"].ToString();
+               var msg  =  MessageBox.Show($"{UpdateMsge} : "+ order,"Order status", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+             
+                if(msg == DialogResult.Yes)
+                {
+                    UpdateOrderStatus(order);
+                    Close();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+           
+
+        }
+
+        private static void UpdateOrderStatus(string Order)
+        {
+            string SourcePath = Path.Combine(OrderDetailsPath,Order + "$A_Copied.txt");
+            string DestPath = Path.Combine(OrderDetailsPath,Order + "$A_Print Completed.txt");
+            File.Move(SourcePath, DestPath);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.SelectedRows[0];
+            orderid = row.Cells[0].Value.ToString();
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                string order = row.Cells[0].Value.ToString();
+                var msg = MessageBox.Show($"Copy : " + order, "Order status", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (msg == DialogResult.Yes)
+                {
+                    Close();
+                    CopyOrder(order);
+                    MessageBox.Show(order + " Copied", "Order status", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            fileSystemWatcher1.Dispose();
+            Application.Exit();
+        }
+        private static void CopyOrder(string order)
+        {
+            ///////////////////
+            string sourcePath = Path.Combine(OrderFilesPath, order);
+            string targetPath = Path.Combine(TodayFolderPathLocal, order);
+                Directory.CreateDirectory(targetPath);
+                foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+                }
+
+                foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+                {
+                    File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+                }
+        }
+        private void Search_Click(object sender, EventArgs e)
+        {
+            var name = TextBox_Name.Text;
+            var order = TextBox_Order_ID.Text;
+            var temp = gridviewdata.Select($"[Folder Name] LIKE '%{name}%' AND [Order ID] LIKE '%{order}%'").CopyToDataTable();
+            dataGridView1.DataSource = temp;
+            dataGridView1.Refresh();
+        }
+
+        private void UpdateComment_Click(object sender, EventArgs e)
+        {
+           string path = Directory.GetFiles(OrderDetailsPath, $"{orderid}*")[0];
+            DialogResult msg = MessageBox.Show($"add comments to order : {orderid}","Message", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (msg == DialogResult.Yes)
+                File.AppendAllText(path, $"Comments : {TextBox_Comments.Text}\n");
+            TextBox_Comments.Text = "";
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            ReadOrders();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (AllowAppClosing == "True")
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
